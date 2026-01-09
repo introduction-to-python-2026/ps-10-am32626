7bb345c81280cb2ba42cc7d5afb1b9fcbe6cbdff
am32626 <ayalamaman00@gmail.com>
2026-01-09 13:48:56 +0200

    Add image loading and edge detection functions
    Implemented load_image function to load and invert image colors. Added edge_detection function to perform edge detection using Sobel filters.

--- image_utils.py
+++ image_utils.py
@@ -3,7 +3,30 @@ import numpy as np
 from scipy.signal import convolve2d
 
 def load_image(path):
-    pass # Replace the `pass` with your code
+    img = Image.open(path)
+    image_np = np.array(img)
+    image_np = 255 - image_np
+    return image_np
+
 
 def edge_detection(image):
-    pass # Replace the `pass` with your code
+   
+    if len(image.shape) == 3:
+        gray = np.mean(image, axis=2)
+    else:
+        gray = image
+    Gx = np.array([
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+    ])
+    Gy = np.array([
+        [-1, -2, -1],
+        [ 0,  0,  0],
+        [ 1,  2,  1]
+    ])
+    edgeX = convolve2d(gray, Gx, mode='same', boundary='fill', fillvalue=0)
+    edgeY = convolve2d(gray, Gy, mode='same', boundary='fill', fillvalue=0)
+    edgeMAG = ((edgeX**2 + edgeY**2) ** 0.5)
+    return edgeMAG
+    
