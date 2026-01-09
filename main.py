424f6282d8492e73526161a0eabde8624497437e
am32626 <ayalamaman00@gmail.com>
2026-01-09 14:00:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 
+ 
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.filters import median
+from skimage.morphology import ball
+from image_utils import load_image, edge_detection
+image = load_image(img.jpg)
+clean_image = median(image, ball(3))
+edge_image = edge_detection(clean_image)
+
+
